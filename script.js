7bd3cef8945ab20becc86b293ab54086f75d442b
Sami Siddiqui <samisiddiqui@airmail.cc>
2022-07-01 10:48:15 -0500

    Finished gameplay logic

--- script.js
+++ script.js
@@ -1,14 +1,78 @@
-const gameboard = (() => {
-    let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    const reset = () => board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    const playMove = (x, y) => {
-        board[x] = y;
+const gameState = (() => {
+    let turnCounter = 0;
+    let gameOver = false;
+
+    const gameboard = (() => {
+        let board = [7, 71, 731, 751, 76, 77, 73, 713, 743];
+        const reset = () => { 
+            board = [7, 71, 731, 751, 76, 77, 73, 713, 743];
+            for (let i = 0; i < gridList.length; i++) {
+                gridList[i].removeAttribute('style');
+            } 
+        };
+        const playMove = (x, y) => {
+            board[x] = y;
+        }
+        const getBoard = () => board;
+        const gridList = document.getElementsByClassName('grid');
+        for (let i = 0; i < gridList.length; i++) {
+            gridList[i].addEventListener('click', function () { test(i) });
+        }
+        function test(i) {
+            if (gameOver) {
+                //This space intentionally left blank
+            } else {
+                if ((gridList[i].getAttribute('style') === 'background-color: red') || (gridList[i].getAttribute('style') === 'background-color: blue')) {
+                    //This space intentionally left blank
+                } else {
+                    gridList[i].setAttribute('style', (turnCounter % 2 === 0) ? 'background-color: red' : 'background-color: blue');
+                    gameboard.playMove(i, (turnCounter % 2 == 0) ? 1 : 2);
+                    turnCounter++;
+                    if (turnCounter >= 4) {
+                        checkIfWin();
+                    }
+                }
+            }
+
+        }
+        function checkIfWin() {
+            //Line win
+            let winStates = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+            for (let z = 0; z < winStates.length; z++) {
+                if (board[winStates[z][0]] == board[winStates[z][1]] & board[winStates[z][1]] == board[winStates[z][2]]) {
+                    gameOver = true;
+                    const res = document.getElementById('result');
+                    res.innerHTML = 'Player ' + board[winStates[z][0]] + ' wins';
+                }
+            }
+            if (turnCounter == 9) {
+                document.getElementById('result').innerHTML = 'Draw';
+                gameOver = true;
+            }
+        }
+
+        return { getBoard, reset, playMove };
+    })();
+
+    const getBoard = () => gameboard.getBoard();
+    const Player = (id, name) => { };
+    const initialize = () => {
+        turnCounter = 0;
+        gameOver = false;
+        gameboard.reset();
+        const p1 = Player(1, 'Alice');
+        const p2 = Player(2, 'Bob');
+        document.getElementById('result').innerHTML = '';
     }
-    const getBoard = () => board;
-    return { reset, playMove, getBoard };
+    
+    return { initialize, getBoard };
 })();
 
-let tc = 1;
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', gameState.initialize);
+
+
+/* let tc = 1;
 
 const gridList = document.getElementsByClassName('grid');
 for (let i = 0; i < gridList.length; i++) {
@@ -30,4 +94,4 @@ button.addEventListener('click', function() {
     for (let i = 0; i < gridList.length; i++) {
         gridList[i].removeAttribute('style');
     }
-});
+}); */
